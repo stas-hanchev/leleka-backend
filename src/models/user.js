70a896373242a006416aec7564909967ddd83f95
Stanislav Hanchev <stanislav@smoothr.de>
2026-01-06 20:03:30 +0200

    soft start

---
+++ src/models/user.js
@@ -0,0 +1,33 @@
+import { model, Schema } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    email: { type: String, unique: true, required: true, trim: true },
+    password: { type: String, required: true, minlength: 8 },
+    avatarUrl: {
+      type: String,
+      required: false,
+      default: "https://ac.goit.global/fullstack/react/default-avatar.jpg",
+    },
+    birthDate: { type: String, required: false },
+    babyGender: { type: String, required: false } // update enum or
+  },
+  { timestamps: true }
+);
+
+userSchema.pre('save', function (next) {
+  if (typeof this.birthDate === 'string') {
+    const [day, month, year] = this.birthDate.split('.');
+    this.birthDate = new Date(`${year}-${month}-${day}`);
+  }
+  next();
+});
+
+userSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
+export const User = model('User', userSchema);

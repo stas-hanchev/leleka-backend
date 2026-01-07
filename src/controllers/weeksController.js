05d5e97dc3d5f2c785fb4dc33c31fd7d2ccaacad
Oksana_Melnyk <kc.19870625@gmail.com>
2026-01-07 15:16:46 +0200

    Update files src

--- src/controllers/weeksController.js
+++ src/controllers/weeksController.js
@@ -1,29 +1,46 @@
+import createHttpError from 'http-errors';
 import { BabyState } from '../models/babyState.js';
 import { MomState } from '../models/momState.js';
-import { getDaysRemaining } from '../utils/calculateDays.js';
-import createHttpError from 'http-errors';
+import { getPregnancyStats } from '../utils/calculatePregnancy.js';
 
 export const getPublicDashboard = async (req, res, next) => {
   try {
     const weekNumber = Number(req.query.week) || 1;
     const babyData = await BabyState.findOne({ weekNumber });
-    res.json({ weekNumber, daysRemaining: 280, babyData });
+
+    if (!babyData) return next(createHttpError(404, 'Тиждень не знайдено'));
+
+    res.json({
+      weekNumber,
+      daysRemaining: 280,
+      babyData
+    });
   } catch (error) { next(error); }
 };
 
 export const getPrivateDashboard = async (req, res, next) => {
+
   try {
-    const { expectedDateOfBirth, currentWeek } = req.user;
-    const daysRemaining = getDaysRemaining(expectedDateOfBirth);
+    const { dueDate } = req.user;
+
+    const { currentWeek, daysRemaining } = getPregnancyStats(dueDate);
+
     const babyData = await BabyState.findOne({ weekNumber: currentWeek });
-    res.json({ weekNumber: currentWeek, daysRemaining, babyData });
+
+    if (!babyData) return next(createHttpError(404, 'Дані для вашого поточного тижня відсутні'));
+
+    res.json({
+      weekNumber: currentWeek,
+      daysRemaining,
+      babyData
+    });
   } catch (error) { next(error); }
 };
 
 export const getBabyDevelopment = async (req, res, next) => {
   try {
-    const { weekNumber } = req.params;
-    const data = await BabyState.findOne({ weekNumber: Number(weekNumber) });
+    const weekNumber = Number(req.params.weekNumber);
+    const data = await BabyState.findOne({ weekNumber });
 
     if (!data) return next(createHttpError(404, 'Дані про розвиток дитини не знайдено'));
 
@@ -37,13 +54,11 @@ export const getBabyDevelopment = async (req, res, next) => {
 
 export const getMomBody = async (req, res, next) => {
   try {
-    const { weekNumber } = req.params;
-    const data = await MomState.findOne({ weekNumber: Number(weekNumber) });
+    const weekNumber = Number(req.params.weekNumber);
+    const data = await MomState.findOne({ weekNumber });
 
     if (!data) return next(createHttpError(404, 'Дані про тіло мами не знайдено'));
 
     res.json(data);
   } catch (error) { next(error); }
 };
-
-
--- src/routes/weeksRoutes.js
+++ src/routes/weeksRoutes.js
@@ -1,14 +1,18 @@
 import { Router } from 'express';
+import { celebrate } from 'celebrate';
 import * as ctrl from '../controllers/weeksController.js';
 import { authenticate } from '../middleware/authenticate.js';
+import { weekNumberSchema, weekQuerySchema } from '../validations/weeks.js';
 
 const router = Router();
 
-router.get('/weeks/public', ctrl.getPublicDashboard);
+router.get('/weeks/public', celebrate(weekQuerySchema), ctrl.getPublicDashboard);
 
 router.use(authenticate);
+
 router.get('/weeks/dashboard', ctrl.getPrivateDashboard);
-router.get('/weeks/:weekNumber/baby', ctrl.getBabyDevelopment);
-router.get('/weeks/:weekNumber/body', ctrl.getMomBody);
+
+router.get('/weeks/:weekNumber/baby', celebrate(weekNumberSchema), ctrl.getBabyDevelopment);
+router.get('/weeks/:weekNumber/body', celebrate(weekNumberSchema), ctrl.getMomBody);
 
 export default router;

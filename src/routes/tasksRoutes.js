bdc8ca8d85387ecab6f026423a9274b615158984
Stanislav Hanchev <stanislav@smoothr.de>
2026-01-10 15:19:49 +0200

    fix

--- src/routes/tasksRoutes.js
+++ src/routes/tasksRoutes.js
@@ -2,7 +2,7 @@
 import { celebrate } from "celebrate";
 import { getAllTasks, createTask, updateTask } from "../controllers/tasksController.js";
 import { createTaskSchema, updateTaskSchema } from "../validations/taskValidation.js";
-import { authenticate } from "../middleware/authenticateTasks.js";
+import { authenticate } from "../middleware/authenticate.js";
 import { Router } from "express";
 
 const tasksRoutes = Router();
